c3473aadeb9c4275eebfff3a1439d11034502673
Julia Hoi <j.x1nyi2@gmail.com>
2022-09-12 21:09:42 +0200

    Add random color function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import { Chalk } from 'chalk';
+import { randomColor } from 'randomcolor';
+import { useState } from 'react';
+
+const chalk = new Chalk({ level: 3 });
+
+// update background color "Generated Color: <hex code>"
 
 function App() {
+  const colorCode = randomColor();
+  const [color, setColor] = useState(colorCode);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="container"
+      style={{
+        backgroundColor: colorCode,
+      }}
+    >
+      <div
+        style={{
+          fontSize: '30px',
+          position: 'relative',
+          top: '35%',
+          display: 'block',
+          justifyContent: 'center',
+          textAlign: 'center',
+          border: 'none',
+          color: 'white',
+          fontWeight: 'bold',
+          margin: 0,
+        }}
+      >
+        Generated Color: <br /> {color}
+      </div>
+
+      <br />
+
+      <button
+        className="btn"
+        onClick={() => {
+          const newRandomColor = colorCode;
+          setColor(newRandomColor);
+        }}
+        style={{
+          position: 'absolute',
+          top: '50%',
+          left: '45%',
+          marginRight: '-50%',
+          marginTop: '25px',
+          width: 'max-content',
+          border: '3px solid white',
+          backgroundColor: 'white',
+          color: colorCode,
+          fontSize: '17px',
+          fontWeight: 'bold',
+          borderRadius: '6px',
+          padding: '8px 15px 8px 15px',
+          transition: 'all 0.5s ease-in-out',
+        }}
+      >
+        GENERATE
+      </button>
     </div>
   );
 }
