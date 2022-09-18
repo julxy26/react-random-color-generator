12dda800917f19c7f51bdeb9d70ab520ca5a4790
Julia Hoi <j.x1nyi2@gmail.com>
2022-09-18 21:18:12 +0200

    Update radio button

--- src/App.js
+++ src/App.js
@@ -4,60 +4,98 @@ import { css } from '@emotion/react';
 import { randomColor } from 'randomcolor';
 import { useState } from 'react';
 
-const colorCode = randomColor();
-
-const containerStyle = css`
-  background-color: ${colorCode};
-  transition: all 0.5s ease-in-out;
-  width: auto;
-  height: 100vh;
-  display: grid;
-`;
-
 const hexStyle = css`
   font-size: 30px;
-  position: static;
-  margin-top: 150px;
-  display: flex;
-  justify-content: center;
   text-align: center;
   font-weight: bold;
   border: none;
   color: white;
+  margin-top: 50px;
 `;
 
 const buttonStyle = css`
-  border: none;
+  border: 1px solid #646a75;
   background-color: white;
-  color: ${colorCode};
+  color: #646a75;
   font-size: 17px;
   font-weight: bold;
-  border-radius: 6px;
-  padding: 10px 17px 10px 17px;
+  border-radius: 14px;
+  margin-top: 40px;
+  padding: 10px 17px;
   transition: all 0.5s ease-in-out;
+  box-shadow: 0 8px 16px 0 rgba(28, 28, 28, 0.3);
 
   &:hover {
     box-shadow: 0 8px 16px 0 rgba(28, 28, 28, 0.3);
     color: white;
     background-color: transparent;
-    border: 2px solid white;
+    border: 1px solid white;
     cursor: pointer;
     transition: 0.5s;
   }
 `;
 
+const inputField = css`
+  position: relative;
+  cursor: text;
+  line-height: 20px;
+  padding: 0 16px;
+  height: 29px;
+  width: 153px;
+  margin: 12px 48px;
+  background-color: #fff;
+  border: 1px solid #d6d6e7;
+  border-radius: 12px;
+  color: rgb(35, 38, 59);
+  box-shadow: inset 0 1px 4px 0 rgb(119 122 175 / 30%);
+  overflow: hidden;
+  transition: all 100ms ease-in-out;
+  display: grid;
+  :focus {
+    border-color: #3c4fe0;
+    box-shadow: 0 1px 0 0 rgb(35 38 59 / 5%);
+  }
+`;
+
 function App() {
+  const colorCode = randomColor();
   const [color, setColor] = useState(colorCode);
-
+  const [userLuminosity, setUserLuminosity] = useState('');
+  const [userHue, setUserHue] = useState('');
+  const userInput = randomColor({
+    hue: userHue,
+    luminosity: userLuminosity,
+  });
   return (
-    <div className="container" css={containerStyle}>
-      <div css={hexStyle}>
-        Generated Color: <br />
-        {color}
-      </div>
+    <div
+      className="container"
+      style={{
+        backgroundColor: color,
+        transition: 'all 0.5s ease-in-out',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <div
+        style={{
+          border: '3px solid white',
+          borderRadius: '8px',
+          padding: '40px',
+          height: '60vh',
+        }}
+      >
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+          }}
+        >
+          <div css={hexStyle}>
+            Generated Color: <br />
+            {color}
+          </div>
 
-      <div>
-        <form>
           <fieldset
             style={{
               border: 'none',
@@ -69,11 +107,53 @@ function App() {
             }}
           >
             <div>
-              <input id="hue" placeholder="type in a hue" />
-              <input type="radio" value="light" name="luminosity" id="light" />
+              <span
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <label htmlFor="hue">Color:</label>
+                <input
+                  css={inputField}
+                  id="hue"
+                  placeholder="type in a hue"
+                  onChange={(event) => {
+                    setUserHue(event.currentTarget.value);
+                  }}
+                />
+              </span>
+              <label htmlFor="luminosity">Luminosity:</label>
+              <input
+                type="radio"
+                value="light"
+                name="luminosity"
+                id="light"
+                onChange={(event) => {
+                  setUserLuminosity(event.currentTarget.value);
+                }}
+              />
               light
-              <input type="radio" value="dark" name="luminosity" id="dark" />
+              <input
+                type="radio"
+                value="dark"
+                name="luminosity"
+                id="dark"
+                onChange={(event) => {
+                  setUserLuminosity(event.currentTarget.value);
+                }}
+              />
               dark
+              <input
+                type="radio"
+                value=""
+                name="luminosity"
+                id="random"
+                onChange={(event) => {
+                  setUserLuminosity(event.currentTarget.value);
+                }}
+              />
+              random
             </div>
           </fieldset>
 
@@ -91,8 +171,7 @@ function App() {
             <button
               className="btn"
               onClick={() => {
-                const newRandomColor = colorCode;
-                setColor(newRandomColor);
+                setColor(userInput);
               }}
               css={buttonStyle}
             >
